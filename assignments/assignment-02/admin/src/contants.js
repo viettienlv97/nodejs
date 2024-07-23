60a3630867b8eec63d2be5012f66202069697b23
Viet Tien <viettienvu.97@gmail.com>
2024-07-24 00:12:23 +0700

    admin assignment 2

---
+++ assignments/assignment-02/admin/src/contants.js
@@ -0,0 +1,12 @@
+export const HTTP_METHOD = {
+  GET: 'GET',
+  POST: 'POST'
+}
+
+export const TRANSACTION_STATUS = {
+  BOOKED: 'Booked',
+  CHECKIN: 'Checkin',
+  CHECKOUT: 'Checkout'
+}
+
+export const API_URL = import.meta.env.VITE_API_URL
